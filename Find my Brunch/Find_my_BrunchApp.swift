4c4d8b3a626914899f5f690182e2f555bb34cfbf
Jordiee1 <chidxmo@gmail.com>
2025-03-24 15:21:30 +0700

    Initial Commit

---
+++ Find my Brunch/Find_my_BrunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_my_BrunchApp.swift
+//  Find my Brunch
+//
+//  Created by จิดาภา สีเพชร on 24/3/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct Find_my_BrunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Find my BrunchTests/Find_my_BrunchTests.swift
@@ -0,0 +1,16 @@
+//
+//  Find_my_BrunchTests.swift
+//  Find my BrunchTests
+//
+//  Created by จิดาภา สีเพชร on 24/3/2568 BE.
+//
+
+import Testing
+
+struct Find_my_BrunchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
